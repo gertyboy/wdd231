0762868757df1feb4e6d2624302cdde1832529ac
Gerrit Thompson <gethompson595@gmail.com>
2024-10-02 11:54:52 -0600

    added update times

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const currentYear = document.getElementById("currentyear")
+const lastUpdated = document.getElementById("last-updated")
+
+currentYear.innerHTML = "&copy"
+
+const lastModified = document.lastModified;
+
+lastUpdated.innerHTML = lastModified
