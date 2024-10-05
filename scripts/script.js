64df6def7307a7f85088fa1f964884fbd99fe3cb
Gerrit Thompson <gethompson595@gmail.com>
2024-10-05 17:37:34 -0600

    added a hamburger

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,21 @@
 const currentYear = document.getElementById("currentyear")
 const lastUpdated = document.getElementById("last-updated")
 
-currentYear.innerHTML = "&copy 2024 - Gerrit Thompson - Rexburg, Idaho, United States of America" 
+currentYear.innerHTML = "&copy 2024 - Gerrit Thompson - Rexburg, Idaho, United States of America"
 
 const lastModified = document.lastModified;
 
 lastUpdated.innerHTML = "Last Update: " + lastModified
+
+
+const menu = document.getElementById("menu")
+
+menu.addEventListener('click', () => {
+    const navigation = document.getElementById("navigation")
+    if (navigation.classList.contains("mobile_hidden")) {
+        navigation.classList.remove("mobile_hidden")
+    } else {
+        navigation.classList.add("mobile_hidden")
+    }
+
+})
