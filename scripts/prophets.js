d411e31f397d7e7d5acfd5b25ca9bdc979c91fc6
Gerrit Thompson <gethompson595@gmail.com>
2024-10-08 14:00:54 -0600

    it worked?

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -1,8 +1,20 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
-
 const cards = document.querySelector('#cards');
 
 async function getProphetData() {
-    await fetch(Response);
-    console.log(Response)
+  try {
+    const response = await fetch(url);
+
+    // Ensure successful response (status code 200)
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 }
+
+getProphetData();
