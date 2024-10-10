a49bd9d4de536aacdfa93af6668bfe8ff0d201e3
Gerrit Thompson <gethompson595@gmail.com>
2024-10-10 14:15:35 -0600

    removed chamber script

---
+++ chamber/scripts/generate.js
@@ -0,0 +1,31 @@
+async function addHeaderAndFooter() {
+    // Create the header element
+    const header = document.createElement('header');
+    const image = document.createElement('img');
+    image.src = 'https://www.spanishfork.org/_assets_/images/logo.png'; // Replace with the actual path to your image
+    image.alt = 'Header image'; // Set an appropriate alternative text
+    const coc = document.createElement('h1')
+    coc.id = "chamber_name"
+    coc.innerHTML = "Spanish Fork Chamber of Commerce"
+
+    // Append the image to the header
+    header.append(image);
+    header.append(coc)
+
+
+    // Create the footer element
+    const footer = document.createElement('footer');
+    footer.textContent = 'This is the footer';
+
+    // Get the body element
+    const body = document.body;
+
+    // Prepend the header to the body
+    body.prepend(header);
+
+    // Append the footer to the body
+    body.append(footer);
+}
+
+// Call the function to add the header and footer
+addHeaderAndFooter();
