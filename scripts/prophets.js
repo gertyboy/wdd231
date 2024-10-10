f54718c481461019469ee174c65b5218679d8a32
Gerrit Thompson <gethompson595@gmail.com>
2024-10-09 21:29:27 -0600

    made things work

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -1,5 +1,5 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
-const cards = document.getElementById("cards")
+const cards = document.querySelector("#cards");
 
 
 
@@ -42,4 +42,4 @@ const displayProphets = (prophets) => {
 }
 
 
-getProphetData();
+getProphetData(url);
