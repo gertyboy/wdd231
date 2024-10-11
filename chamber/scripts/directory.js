4aa7584d0927992cfbb94f22ebbc550ffe6c3b8e
Gerrit Thompson <gethompson595@gmail.com>
2024-10-10 22:50:02 -0600

    made cleaner urls

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -22,17 +22,25 @@ async function getBusinessData(url) {
 
 
 const displayBusinesses = (businesses) => {
-    
-        businesses.forEach((business) => {
-        // Create elements to add to the div.cards element
-        let card = document.createElement('section');
-        let businessName = document.createElement('h2'); // fill in the blank
-        let portrait = document.createElement('img');
 
-        console.log(business.imageurl)
+    businesses.forEach((business) => {
+        // Create elements to add to the div.cards element
+        const card = document.createElement('section');
+        const businessName = document.createElement('h2'); // fill in the blank
+        const firstp = document.createElement('p')
+        const secondp = document.createElement('p')
+        const thirdp = document.createElement('a')
+        const portrait = document.createElement('img');
         portrait.setAttribute('src', business.imageurl);
-        portrait.url = businesses.imageurl;
         card.append(portrait);
+        businessName.innerHTML = business.name;
+        card.append(businessName);
+        firstp.innerHTML = business.address
+        card.append(firstp)
+        secondp.innerHTML = business.number
+        card.append(secondp)
+        thirdp.innerHTML = business.url
+        card.append(thirdp)
         cards.appendChild(card);
     }); // end of arrow function and forEach loop
 }
