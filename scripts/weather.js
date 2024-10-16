b2d06952510fc47d18ee0ec10ac9dc311192406b
Gerrit Thompson <gethompson595@gmail.com>
2024-10-15 20:19:57 -0600

    Added h1

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,10 +2,11 @@ const myTown = document.querySelector('#town');
 const myDecription = document.querySelector('#description');
 const myTemperature = document.querySelector('#temperature');
 const myGraphic = document.querySelector('#graphic');
+const currentTemp = document.getElementById('current-temp')
 // CREATE REQUIED VARIABLES FOR THE URL
 const myKey = "c6ff61a3a0944fdbe6d03222dfec51d0";
-const myLat = "42.91";
-const myLong = "-76.80";
+const myLat = "49.75";
+const myLong = "6.64";
 //CONSTRUCT A FULL PATH USING TEMPLATE LITERALS
 const myURL = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
 //I TRY TO GRAB THE CURRENT WEATHER DATA
@@ -15,8 +16,9 @@ async function apiFetch() {
     if (response.ok) {
       const data = await response.json();
       console.log(data); // testing only
+      let temperature = Math.round(data.main.temp);
+      currentTemp.innerHTML = temperature
     }
-    // displayResults(data) : // uncomment when ready
     else {
       throw Error(await response.text());
     }
